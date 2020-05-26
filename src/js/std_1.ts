f6798ecc940b5efecd9c9199e925b316a7148be8
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2020-05-26 15:10:08 +0700

    js/std first example

---
+++ src/js/std_1.ts
@@ -0,0 +1,37 @@
+type Offer = {
+    offerName: string;
+    offerType: string;
+    offerParams: {
+        reason: string;
+        buttonName: string;
+        product_type: string;
+    };
+};
+type OfferFormValue = {
+    [key in keyof Offer['offerParams']]: string;
+};
+
+const offer: Offer = {
+    offerName: 'Платинум',
+    offerType: 'promo_product_mb',
+    offerParams: {
+        buttonName: 'Оформить карту',
+        product_type: 'platinum-card-new',
+        reason: 'Не требуется|Не интересно|Не сейчас|Другое',
+    },
+};
+
+const returningValue: OfferFormValue = {
+    reason: 'Не требуется',
+    buttonName: 'Оформить карту',
+    product_type: 'platinum-card-new',
+};
+
+const makeOfferFormValue = (offer: Offer): OfferFormValue => {
+    return offer;
+};
+
+const formValue = makeOfferFormValue(offer);
+
+console.log(formValue);
+console.log(returningValue);
