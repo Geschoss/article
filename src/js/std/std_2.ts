29b6fd077024d4411bfd59279e29f79397998b64
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2020-06-12 15:29:15 +0700

    immutable

--- src/js/std/std_2.ts
+++ src/js/std/std_2.ts
@@ -9,3 +9,20 @@ b.name = 'boris';
 
 console.log(a);
 console.log(b);
+
+const offerParams = {
+    buttonName: 'Оформить карту',
+    product_type: 'platinum-card-new',
+    reason: 'Не требуется|Не интересно|Не сейчас|Другое',
+};
+
+const convertOfferToInitialValue = (offerParams) => {
+    offerParams.reason = offerParams.reason.split('|')[0];
+
+    return offerParams;
+};
+
+const initialValue = convertOfferToInitialValue(offerParams);
+
+console.log(initialValue);
+console.log(offerParams);
