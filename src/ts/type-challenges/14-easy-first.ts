10ff676530549e62289a4a41603fbf9b8c3fe7eb
Pavel Kolomnikov <pekolomnikov@avito.ru>
2020-12-18 18:54:57 +0700

    type challeges | easy-first update

--- src/ts/type-challenges/14-easy-first.ts
+++ src/ts/type-challenges/14-easy-first.ts
@@ -1,7 +1,11 @@
 type arr1 = ['a', 'b', 'c']
 type arr2 = [3, 2, 1]
+type arr3 = [3]
+type arr4 = []
 
-type First<T extends ReadonlyArray<any>> = T[0];
+type First<T extends ReadonlyArray<any>> = T extends never[] ? never : T[0];
 
 type head1 = First<arr1> // expected to be 'a'
 type head2 = First<arr2> // expected to be 3
+type head3 = First<arr3> // expected to be 3
+type head4 = First<arr4> // expected to be never
