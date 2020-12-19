fb1fab2e03617a571c86ce4fbc9508d88d304810
Pavel Kolomnikov <pekolomnikov@avito.ru>
2020-12-20 00:51:43 +0700

    type challeges | omit

---
+++ src/ts/type-challenges/3-medium-omit.ts
@@ -0,0 +1,13 @@
+interface TodoOmit {
+    title: string;
+    description: string;
+    completed: boolean;
+}
+
+type MyOmit<T, K extends keyof T> = Pick<T, MyExclude<keyof T, K>>
+
+type TodoPreview = MyOmit<TodoOmit, 'description' | 'title'>;
+
+const todoOmit: TodoPreview = {
+    completed: false,
+};
