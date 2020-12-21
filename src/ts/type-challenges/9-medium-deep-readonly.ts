f9b037fbd333e0d01955739a63490dcbb920f855
Pavel Kolomnikov <pekolomnikov@avito.ru>
2020-12-21 16:59:52 +0700

    type challeges | deep-readonly

---
+++ src/ts/type-challenges/9-medium-deep-readonly.ts
@@ -0,0 +1,22 @@
+type X9 = {
+    x: {
+        a: number;
+        b: string;
+    };
+    y: string;
+};
+
+type DeepReadonly<T> = T extends never
+    ? T
+    : { readonly [K in keyof T]: DeepReadonly<T[K]> };
+
+const todo9: DeepReadonly<X9> = {
+    x: {
+        a: 1,
+        b: 'hi',
+    },
+    y: 'hey',
+};
+
+todo9.y = 'hello';
+todo9.x.b = 'hello';
