ce350a42f2b2e5dfda2510a4535f4e4d57664764
Geschoss <q11254@gmail.com>
2022-01-14 17:55:30 +0700

    gasp | init

---
+++ packages/grasp/CouplingCohesion/1-bad/logger.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const writeLog = (req, res) => () => {
+    const time = Date.now() - req.requestTime;
+
+    console.log(`${req.method} ${req.url} ${req.headers.referere} ${time}`);
+}
+
+const logger = (req, res, next) => {
+    req.requestTime = Date.now();
+    res.on('close', writeLog(req, res));
+    next();
+}
+
+module.exports = logger;
