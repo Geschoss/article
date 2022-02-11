932919d7d13f8927f642bff12047cfcbe5c580d5
Pavel Kolomnikov <q11254@gmail.com>
2022-02-11 19:37:37 +0700

    node | ws

---
+++ packages/HowProgrammingWorks/node/httpRequest/1-get.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const http = require('http');
+
+const url = 'https://ietf.org/';
+
+http.get(url, res => {
+	console.log(res.req._header);
+	console.dir(res.headers);
+	if (res.statusCode !== 200) {
+		const { statusCode, statusMessage } = res;
+		console.log(`Status Code: ${statusCode} ${statusMessage}`);
+		return;
+	}
+
+	res.setEncoding('utf8');
+	const buffer = [];
+	res.on('data', chunk => {
+		buffer.push(chunk);
+	});
+	res.on('end', () => {
+		console.log(buffer.join());
+	});
+});
