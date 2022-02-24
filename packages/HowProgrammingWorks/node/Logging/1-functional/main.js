e10f67c658a34e86be988d32695c559a9d485533
Pavel Kolomnikov <q11254@gmail.com>
2022-02-24 16:54:43 +0700

    node | logger

---
+++ packages/HowProgrammingWorks/node/Logging/1-functional/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const logger = require('./logger.js');
+
+const application = {};
+// application.logger = logger(process.stdout)()('app1');
+application.logger = logger('./file.log')(JSON.stringify)('app1');
+
+// In module1
+
+const module1 = {};
+module1.logger = application.logger('module1');
+
+// Create functions
+
+const info = module1.logger('info');
+const warning = module1.logger('warning');
+const error = module1.logger('error');
+const debug = module1.logger('debug');
+
+// Usage
+
+info('I have info for you');
+warning('Hello there!');
+error('World is not found');
+debug('Bye!');
