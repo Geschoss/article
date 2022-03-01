679706b4478a12349e5ef493d161d6f3798da0b2
Pavel Kolomnikov <q11254@gmail.com>
2022-03-01 15:52:35 +0700

    node | contract

---
+++ packages/HowProgrammingWorks/node/Contract/4-endpoint.mjs
@@ -0,0 +1,18 @@
+// server
+
+({
+  parameters: {
+    a: 'number',
+    b: 'number',
+  },
+  method: async ({ a, b }) => {
+    const result = a > b;
+    return result;
+  },
+
+  returns: 'boolean',
+});
+
+// front
+
+const res = await api.example.compare({ a: 1, b: 2 })
