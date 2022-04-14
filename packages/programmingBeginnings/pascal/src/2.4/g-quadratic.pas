8fd2ac44db898b7e79d56456a2e038189789a971
Pavel Kolomnikov <q11254@gmail.com>
2022-04-14 15:17:26 +0700

    some pascal

---
+++ packages/programmingBeginnings/pascal/src/2.4/g-quadratic.pas
@@ -0,0 +1,22 @@
+program f1
+procedure quadratic(a, b, c: real; var ok: boolean; var x1, x2: real);
+var
+  d: real;
+begin
+  ok := false;
+  if a = 0 then
+    exit
+
+  d := b*b - 4*a*c;
+  if d < 0 then
+    ok := false
+  else
+  begin
+    d := sqrt(d);
+    x1 := (-b - d) / (2*a);
+    x2 := (-b + d) / (2*a);
+    ok := true
+  end
+end;
+
+end.
