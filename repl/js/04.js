b7589c63679b88e0c3c05c90ff229ac1d8222c09
Pavel Kolomnikov <q11254@gmail.com>
2022-11-27 18:02:25 +0700

    repl add files

--- repl/js/04.js
+++ repl/js/04.js
@@ -1,2 +1,25 @@
-let result = Math.acos(10 / 13)
-result
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+ var isIsomorphic = function(s, t) {
+  if ((new Set(s)).size !== (new Set(t)).size) {
+    return false;
+  }
+  let linked_chars = {};
+  for (let i = 0; i < s.length; i++) {
+    let s_char = s[i];
+    let t_char = t[i];
+    let linked_char = linked_chars[s_char];
+    if (linked_char) {
+      if (linked_char === t_char) {
+        continue;
+      }
+      return false;
+    } else {
+      linked_chars[s_char] = t_char;
+    }
+  }
+  return true;
+};
