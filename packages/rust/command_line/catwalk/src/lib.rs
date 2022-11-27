40b6f85448184ec2d398534aeceba77ec3f7575b
Pavel Kolomnikov <q11254@gmail.com>
2022-11-27 17:41:44 +0700

    rust cli

--- packages/rust/command_line/catwalk/src/lib.rs
+++ packages/rust/command_line/catwalk/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
 pub struct Config {
@@ -10,8 +12,37 @@ pub struct Config {
 
 type TestResult<T> = Result<T, Box<dyn Error>>;
 pub fn run(config: Config) -> TestResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if config.number_lines {
+                        println!("{:6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> TestResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> TestResult<Config> {
@@ -20,29 +51,31 @@ pub fn get_args() -> TestResult<Config> {
         .author("pako")
         .about("Rust cat")
         .arg(
-            Arg::with_name("number_nonblank_lines")
-                .short("n")
-                .help("number all output lines")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("number_lines")
-                .short("b")
-                .help("number nonempty output lines")
-                .takes_value(false),
-        )
-        .arg(
             Arg::with_name("files")
                 .value_name("FILE")
                 .help("input files")
-                .required(true)
-                .min_values(1),
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .help("number all output lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("number nonempty output lines")
+                .takes_value(false),
         )
         .get_matches();
 
     Ok(Config {
         files: matcher.values_of_lossy("files").unwrap(),
-        number_lines: matcher.is_present("number_lines"),
-        number_nonblank_lines: matcher.is_present("number_nonblank_lines"),
+        number_lines: matcher.is_present("number"),
+        number_nonblank_lines: matcher.is_present("number_nonblank"),
     })
 }
