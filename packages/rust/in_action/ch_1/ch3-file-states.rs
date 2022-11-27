be0328d71186c90d72dbf88eba29730000e5af67
Pavel Kolomnikov <q11254@gmail.com>
2022-11-27 13:59:37 +0700

    rust in action

---
+++ packages/rust/in_action/ch_1/ch3-file-states.rs
@@ -0,0 +1,29 @@
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    } 
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
+        let mut tmp = self.data.clone();
+        
+    }
+}
